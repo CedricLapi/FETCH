34873062da4d2c8dc978aeb6f244364718fe86b5
Cedric Lapi <lapicedric@gmail.com>
2024-01-27 14:42:03 +0100

    More Details!

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,23 @@ function App() {
 
   const [users, setUsers] = useState([]);
 
+  //Run side-effects when a component is mounted.
+  //useEffect(() => {}, []), a higher order function encapsulating a callback function.
   useEffect(() => {
+
+    //Fetch is basically a promise, allowing us to run stuff asynchronously, that is simultaneously.
     fetch(base_url + "users?page=2")
-    .then(response => response.json())
+    .then(response => response.json()) //What to do when things go well.
 
     .then(json => setUsers(json.data))
 
-    .catch(error => console.log(error))
-  }, []);
+    .catch(error => console.log(error)) //What to do when it goes sideways.
+
+     //Dependency array: You'll never want to use your setter on the same useEffect that is depended on your getter.
+     //otherwise, you'll get into some sort of infinite loop where the depedency is updated continuously.
+     //Having it empty, means the side-effect would be run only once.
+
+  }, [users]);
   return (
     <>
       {users.map(user =>{
@@ -24,7 +33,7 @@ function App() {
 
           <h1>{user.first_name}</h1>
           <h1>{user.last_name}</h1>
-          <img src={user.avatar} alt={user.first_name} />
+          <img src={user.avatar} alt="user avatar" />
         </div>
       })}
     </>
